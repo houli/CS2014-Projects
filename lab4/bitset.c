96b757e04418fa74893caa4eea2807fb34717f07
Eoin Houlihan <ehoulih@tcd.ie>
2014-10-29 11:06:24 +0000

    Use calloc instead of iterating. Main function and print charset implemented.

--- lab4/bitset.c
+++ lab4/bitset.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 const int INT_SIZE = sizeof(unsigned int) * 8;
 
@@ -19,12 +20,8 @@ bitset * bitset_new(int size) {
     set->size++;
   }
 
-  set->array = malloc(set->size * sizeof(unsigned int));
+  set->array = calloc(set->size, sizeof(unsigned int));
 
-  int i;
-  for (i = 0; i < set->size; i++) {
-    set->array[i] = 0;
-  }
   return set;
 }
 
@@ -44,7 +41,7 @@ int bitset_lookup(bitset * this, int item) {
   if (out_of_bounds(this, item)) {
     return -1;
   }
-  int index = item / (this->size * INT_SIZE);
+  int index = item / INT_SIZE;
   int bit_offset = item % INT_SIZE;
 
   return this->array[index] & (1 << bit_offset);
@@ -57,7 +54,7 @@ int bitset_add(bitset * this, int item) {
   if (out_of_bounds(this, item)) {
     return 0;
   }
-  int index = item / (this->size * INT_SIZE);
+  int index = item / INT_SIZE;
   int bit_offset = item % INT_SIZE;
 
   this->array[index] |= (1 << bit_offset);
@@ -70,7 +67,7 @@ int bitset_remove(bitset * this, int item) {
   if (out_of_bounds(this, item)) {
     return 0;
   }
-  int index = item / (this->size * INT_SIZE);
+  int index = item / INT_SIZE;
   int bit_offset = item % INT_SIZE;
 
   this->array[index] &= (~(1 << bit_offset));
@@ -93,13 +90,59 @@ void bitset_intersect(bitset * dest, bitset * src1, bitset * src2) {
   }
 }
 
-int main() {
-  bitset * set = bitset_new(300);
-  bitset_add(set, 20);
-  if (bitset_lookup(set, 20)) {
-    printf("In there!\n");
-  } else {
-    printf("Not in there!\n");
+void print_charset(bitset * set) {
+  printf("{");
+  int is_first = 1;
+  int i;
+  for (i = 0; i < 256; i++) {
+    if (bitset_lookup(set, i)) {
+    if (!is_first) {
+      printf(", ");
+    } else {
+      is_first = 0;
+    }
+      printf("\"%c\"", i);
+    }
   }
+  printf("}\n");
+}
+
+int main() {
+  printf("Type in some characters:\n");
+  int buf_size = 1024;
+  char * str1 = malloc(buf_size * sizeof(char));
+  scanf("%[^\n]%*c", str1);
+  int len = strlen(str1);
+  bitset * set1 = bitset_new(256);
+  int i;
+  for (i = 0; i < len; i++) {
+    bitset_add(set1, (int)str1[i]);
+  }
+
+  printf("\nType in some more characters:\n");
+  char * str2 = malloc(buf_size * sizeof(char));
+  scanf("%[^\n]%*c", str2);
+  len = strlen(str2);
+  bitset * set2 = bitset_new(256);
+  for (i = 0; i < len; i++) {
+    bitset_add(set2, (int)str2[i]);
+  }
+
+  printf("\nFirst character set:\n");
+  print_charset(set1);
+
+  printf("\nSecond character set:\n");
+  print_charset(set2);
+
+  bitset * union_set = bitset_new(256);
+  bitset_union(union_set, set1, set2);
+  printf("\nUnion of first two character sets:\n");
+  print_charset(union_set);
+
+  bitset * intersect_set = bitset_new(256);
+  bitset_intersect(intersect_set, set1, set2);
+  printf("\nIntersection of first two character sets:\n");
+  print_charset(intersect_set);
+
   return 0;
 }
